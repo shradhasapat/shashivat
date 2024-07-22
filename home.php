9e8769017fb3d59be4d280b18102a0f95516af55
shradhasapat <shradhasapat191991@gmail.com>
2024-07-22 16:47:06 +0530

    comments

--- home.php
+++ home.php
@@ -40,6 +40,22 @@
 
 	<!-- bootstrap -->
 	<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css" integrity="sha384-xOolHFLEh07PJGoPkLv1IbcEPTNtaed2xpHsD9ESMhqIYd0nLMwNLD69Npy4HI+N" crossorigin="anonymous">
+	<!-- bootstrap -->
+	<!-- bootstrap -->
+	<!-- bootstrap -->
+	<!-- bootstrap -->
+	<!-- bootstrap -->
+	<!-- bootstrap -->
+	<!-- bootstrap -->
+	<!-- bootstrap -->
+	<!-- bootstrap -->
+	<!-- bootstrap -->
+	<!-- bootstrap -->
+	<!-- bootstrap -->
+	<!-- bootstrap -->
+	<!-- bootstrap -->
+	<!-- bootstrap -->
+	<!-- bootstrap -->
 
 	<style>
 		.car_caption{
