1c9d3731461fe52a631e75357ed0747fd7d64db2
shradhasapat <84843316+shradhasapat@users.noreply.github.com>
2024-07-22 15:46:10 +0530

    Add files via upload

---
+++ blog-classic.php
@@ -0,0 +1,283 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="keywords" content="" />
+	<meta name="author" content="" />
+	<meta name="robots" content="" />
+	<meta name="description" content="JobBoard - HTML Template" />
+	<meta property="og:title" content="JobBoard - HTML Template" />
+	<meta property="og:description" content="JobBoard - HTML Template" />
+	<meta property="og:image" content="JobBoard - HTML Template" />
+	<meta name="format-detection" content="telephone=no">
+	
+	<!-- FAVICONS ICON -->
+	<link rel="icon" href="images/favicon.ico" type="image/x-icon" />
+	<link rel="shortcut icon" type="image/x-icon" href="images/favicon.png" />
+	
+	<!-- PAGE TITLE HERE -->
+	<title>JobBoard - HTML Template</title>
+	
+	<!-- MOBILE SPECIFIC -->
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	
+	<!--[if lt IE 9]>
+	<script src="js/html5shiv.min.js"></script>
+	<script src="js/respond.min.js"></script>
+	<![endif]-->
+	
+	<!-- STYLESHEETS -->
+	<link rel="stylesheet" type="text/css" href="css/plugins.css">
+	<link rel="stylesheet" type="text/css" href="css/style.css">
+	<link rel="stylesheet" type="text/css" href="css/templete.css">
+	<link class="skin" rel="stylesheet" type="text/css" href="css/skin/skin-1.css">
+
+</head>
+<body id="bg">
+<div class="page-wraper">
+<div id="loading-area"></div>
+	<!-- header -->
+	<?php 
+	require_once "header.php";
+?>
+    <!-- header END -->
+    <!-- Content -->
+    <div class="page-content bg-white">
+        <!-- inner page banner -->
+        <div class="dez-bnr-inr overlay-black-middle" style="background-image:url(images/banner/bnr1.jpg);">
+            <div class="container">
+                <div class="dez-bnr-inr-entry">
+                    <h1 class="text-white">Blog Classic</h1>
+					<!-- Breadcrumb row -->
+					<div class="breadcrumb-row">
+						<ul class="list-inline">
+							<li><a href="#">Home</a></li>
+							<li>Blog Classic</li>
+						</ul>
+					</div>
+					<!-- Breadcrumb row END -->
+                </div>
+            </div>
+        </div>
+        <!-- inner page banner END -->
+        <!-- contact area -->
+        <div class="content-area">
+            <div class="container max-w900">
+                <!-- Blog large img -->
+                <div class="blog-post blog-lg blog-style-1">
+                    <div class="dez-post-media dez-img-effect zoom-slow radius-sm"> <a href="blog-details.html"><img src="images/blog/default/thum2.jpg" alt=""></a> </div>
+                    <div class="dez-info">
+						 <div class="dez-post-meta">
+							<ul class="d-flex align-items-center">
+								<li class="post-date"><i class="fa fa-calendar"></i>September 18, 2020</li>
+								<li class="post-author"><i class="fa fa-user"></i>By <a href="#">demongo</a> </li>
+								<li class="post-comment"><i class="fa fa-comments-o"></i><a href="#">5k</a> </li>
+							</ul>
+						</div>
+						<div class="dez-post-title">
+							<h4 class="post-title font-24"><a href="blog-details.html">Do you have a job that the average person doesn’t even know exists?</a></h4>
+						</div>
+						<div class="dez-post-text">
+                            <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy 
+                                text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
+                        </div>
+						<div class="dez-post-readmore blog-share"> 
+							<a href="blog-details.html" title="READ MORE" rel="bookmark" class="site-button-link"><span class="fw6">READ MORE</span></a>
+						</div>
+					</div>
+                </div>
+                <div class="blog-post blog-lg blog-style-1">
+                    <div class="dez-post-media dez-img-effect zoom-slow radius-sm"> <a href="blog-details.html"><img src="images/blog/default/thum1.jpg" alt=""></a> </div>
+                    <div class="dez-info">
+						<div class="dez-post-meta">
+							<ul class="d-flex align-items-center">
+								<li class="post-date"><i class="fa fa-calendar"></i>September 18, 2020</li>
+								<li class="post-author"><i class="fa fa-user"></i>By <a href="#">demongo</a> </li>
+								<li class="post-comment"><i class="fa fa-comments-o"></i><a href="#">5k</a> </li>
+							</ul>
+						</div>
+						<div class="dez-post-title">
+							<h4 class="post-title font-24"><a href="blog-details.html">Do you have a job that the average person doesn’t even know exists?</a></h4>
+						</div>
+						<div class="dez-post-text">
+                            <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy 
+                                text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
+                        </div>
+						<div class="dez-post-readmore blog-share"> 
+							<a href="blog-details.html" title="READ MORE" rel="bookmark" class="site-button-link"><span class="fw6">READ MORE</span></a>
+						</div>
+					</div>
+                </div>
+                <div class="blog-post blog-lg blog-style-1">
+                    <div class="dez-post-media dez-img-effect zoom-slow radius-sm"> <a href="blog-details.html"><img src="images/blog/default/thum4.jpg" alt=""></a> </div>
+                    <div class="dez-info">
+						 <div class="dez-post-meta">
+							<ul class="d-flex align-items-center">
+								<li class="post-date"><i class="fa fa-calendar"></i>September 18, 2020</li>
+								<li class="post-author"><i class="fa fa-user"></i>By <a href="#">demongo</a> </li>
+								<li class="post-comment"><i class="fa fa-comments-o"></i><a href="#">5k</a> </li>
+							</ul>
+						</div>
+						<div class="dez-post-title ">
+							<h4 class="post-title font-24"><a href="blog-details.html">Do you have a job that the average person doesn’t even know exists?</a></h4>
+						</div>
+						<div class="dez-post-text">
+                            <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy 
+                                text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
+                        </div>
+						<div class="dez-post-readmore blog-share"> 
+							<a href="blog-details.html" title="READ MORE" rel="bookmark" class="site-button-link"><span class="fw6">READ MORE</span></a>
+						</div>
+					</div>
+                </div>
+                <div class="blog-post blog-lg blog-style-1">
+                    <div class="dez-post-media dez-img-effect zoom-slow radius-sm"> <a href="blog-details.html"><img src="images/blog/default/thum3.jpg" alt=""></a> </div>
+                    <div class="dez-info">
+						 <div class="dez-post-meta">
+							<ul class="d-flex align-items-center">
+								<li class="post-date"><i class="fa fa-calendar"></i>September 18, 2020</li>
+								<li class="post-author"><i class="fa fa-user"></i>By <a href="#">demongo</a> </li>
+								<li class="post-comment"><i class="fa fa-comments-o"></i><a href="#">5k</a> </li>
+							</ul>
+						</div>
+						<div class="dez-post-title ">
+							<h4 class="post-title font-24"><a href="blog-details.html">Do you have a job that the average person doesn’t even know exists?</a></h4>
+						</div>
+						<div class="dez-post-text">
+                            <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy 
+                                text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
+                        </div>
+						<div class="dez-post-readmore blog-share"> 
+							<a href="blog-details.html" title="READ MORE" rel="bookmark" class="site-button-link"><span class="fw6">READ MORE</span></a>
+						</div>
+					</div>
+                </div>
+                <div class="blog-post blog-lg blog-style-1">
+                    <div class="dez-post-media dez-img-effect zoom-slow radius-sm"> <a href="blog-details.html"><img src="images/blog/default/thum2.jpg" alt=""></a> </div>
+                    <div class="dez-info">
+						 <div class="dez-post-meta">
+							<ul class="d-flex align-items-center">
+								<li class="post-date"><i class="fa fa-calendar"></i>September 18, 2020</li>
+								<li class="post-author"><i class="fa fa-user"></i>By <a href="#">demongo</a> </li>
+								<li class="post-comment"><i class="fa fa-comments-o"></i><a href="#">5k</a> </li>
+							</ul>
+						</div>
+						<div class="dez-post-title ">
+							<h4 class="post-title font-24"><a href="blog-details.html">Do you have a job that the average person doesn’t even know exists?</a></h4>
+						</div>
+						<div class="dez-post-text">
+                            <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy 
+                                text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
+                        </div>
+						<div class="dez-post-readmore blog-share"> 
+							<a href="blog-details.html" title="READ MORE" rel="bookmark" class="site-button-link"><span class="fw6">READ MORE</span></a>
+						</div>
+					</div>
+                </div>
+                <div class="blog-post blog-lg blog-style-1">
+                    <div class="dez-post-media dez-img-effect zoom-slow radius-sm"> <a href="blog-details.html"><img src="images/blog/default/thum1.jpg" alt=""></a> </div>
+                    <div class="dez-info">
+						 <div class="dez-post-meta">
+							<ul class="d-flex align-items-center">
+								<li class="post-date"><i class="fa fa-calendar"></i>September 18, 2020</li>
+								<li class="post-author"><i class="fa fa-user"></i>By <a href="#">demongo</a> </li>
+								<li class="post-comment"><i class="fa fa-comments-o"></i><a href="#">5k</a> </li>
+							</ul>
+						</div>
+						<div class="dez-post-title ">
+							<h4 class="post-title font-24"><a href="blog-details.html">Do you have a job that the average person doesn’t even know exists?</a></h4>
+						</div>
+						<div class="dez-post-text">
+                            <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy 
+                                text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
+                        </div>
+						<div class="dez-post-readmore blog-share"> 
+							<a href="blog-details.html" title="READ MORE" rel="bookmark" class="site-button-link"><span class="fw6">READ MORE</span></a>
+						</div>
+					</div>
+                </div>
+                <!-- Blog large img END -->
+                <!-- Pagination start -->
+                <div class="pagination-bx clearfix text-center">
+					<ul class="pagination">
+						<li class="previous"><a href="javascript:void(0);"><i class="ti-arrow-left"></i> Prev</a></li>
+						<li class="active"><a href="#">1</a></li>
+						<li><a href="javascript:void(0);">2</a></li>
+						<li><a href="javascript:void(0);">3</a></li>
+						<li class="next"><a href="javascript:void(0);">Next <i class="ti-arrow-right"></i></a></li>
+					</ul>
+				</div>
+                <!-- Pagination END -->
+            </div>
+        </div>
+    </div>
+    <!-- Content END-->
+	<!-- Modal Box -->
+	<div class="modal fade lead-form-modal" id="car-details" tabindex="-1" role="dialog" >
+		<div class="modal-dialog" role="document">
+			<div class="modal-content">
+				<button type="button" class="close" data-dismiss="modal" aria-label="Close">
+					<span aria-hidden="true">&times;</span>
+				</button>
+				<div class="modal-body row m-a0 clearfix">
+					<div class="col-lg-6 col-md-6 overlay-primary-dark d-flex p-a0" style="background-image: url(images/background/bg3.jpg);  background-position:center; background-size:cover;">
+						<div class="form-info text-white align-self-center">
+							<h3 class="m-b15">Login To You Now</h3>
+							<p class="m-b15">Lorem Ipsum is simply dummy text of the printing and typesetting industry has been the industry.</p>
+							<ul class="list-inline m-a0">
+								<li><a href="#" class="m-r10 text-white"><i class="fa fa-facebook"></i></a></li>
+								<li><a href="#" class="m-r10 text-white"><i class="fa fa-google-plus"></i></a></li>
+								<li><a href="#" class="m-r10 text-white"><i class="fa fa-linkedin"></i></a></li>
+								<li><a href="#" class="m-r10 text-white"><i class="fa fa-instagram"></i></a></li>
+								<li><a href="#" class="m-r10 text-white"><i class="fa fa-twitter"></i></a></li>
+							</ul>
+						</div>
+					</div>
+					<div class="col-lg-6 col-md-6 p-a0">
+						<div class="lead-form browse-job text-left">
+							<form>
+								<h3 class="m-t0">Personal Details</h3>
+								<div class="form-group">
+									<input value="" class="form-control" placeholder="Name"/>
+								</div>	
+								<div class="form-group">
+									<input value="" class="form-control" placeholder="Mobile Number"/>
+								</div>
+								<div class="clearfix">
+									<button type="button" class="btn-primary site-button btn-block">Submit </button>
+								</div>
+							</form>
+						</div>
+					</div>
+				</div>	
+			</div>
+		</div>
+	</div>
+	<!-- Modal Box End -->
+	<!-- Footer -->
+	<?php 
+	require_once "footer.php";
+?>
+    <!-- Footer END -->
+    <!-- scroll top button -->
+    <button class="scroltop fa fa-chevron-up" ></button>
+</div>
+<!-- JAVASCRIPT FILES ========================================= -->
+<script src="js/jquery.min.js"></script><!-- JQUERY.MIN JS -->
+<script src="plugins/bootstrap/js/popper.min.js"></script><!-- BOOTSTRAP.MIN JS -->
+<script src="plugins/bootstrap/js/bootstrap.min.js"></script><!-- BOOTSTRAP.MIN JS -->
+<script src="plugins/bootstrap-select/bootstrap-select.min.js"></script><!-- FORM JS -->
+<script src="plugins/bootstrap-touchspin/jquery.bootstrap-touchspin.js"></script><!-- FORM JS -->
+<script src="plugins/magnific-popup/magnific-popup.js"></script><!-- MAGNIFIC POPUP JS -->
+<script src="plugins/counter/waypoints-min.js"></script><!-- WAYPOINTS JS -->
+<script src="plugins/counter/counterup.min.js"></script><!-- COUNTERUP JS -->
+<script src="plugins/imagesloaded/imagesloaded.js"></script><!-- IMAGESLOADED -->
+<script src="plugins/masonry/masonry-3.1.4.js"></script><!-- MASONRY -->
+<script src="plugins/masonry/masonry.filter.js"></script><!-- MASONRY -->
+<script src="plugins/owl-carousel/owl.carousel.js"></script><!-- OWL SLIDER -->
+<script src="js/custom.js"></script><!-- CUSTOM FUCTIONS  -->
+<script src="js/dz.carousel.js"></script><!-- SORTCODE FUCTIONS  -->
+<script src="js/dz.ajax.js"></script><!-- CONTACT JS  -->
+</body>
+</html>
